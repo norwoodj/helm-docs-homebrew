5d0a9f37627f191a98b70cfd5c108e15fdbdfd0e
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-30 01:38:16 +0000

    Brew formula update for helm-docs version v0.11.0

--- Formula/helm-docs.rb
+++ Formula/helm-docs.rb
@@ -2,14 +2,14 @@
 class HelmDocs < Formula
   desc "Automatically generate markdown documentation for helm charts"
   homepage "https://github.com/norwoodj/helm-docs"
-  version "0.10.0"
+  version "0.11.0"
 
   if OS.mac?
-    url "https://github.com/norwoodj/helm-docs/releases/download/v0.10.0/helm-docs_0.10.0_Darwin_x86_64.tar.gz"
-    sha256 "ca16bf861623707702ec48bc2d69e9fed701a2203ed2e109595f412677e98501"
+    url "https://github.com/norwoodj/helm-docs/releases/download/v0.11.0/helm-docs_0.11.0_Darwin_x86_64.tar.gz"
+    sha256 "bc1261714e7d0be7c06501798ba8e48dc825b49c05561a2804834ebfc202b6de"
   elsif OS.linux?
-    url "https://github.com/norwoodj/helm-docs/releases/download/v0.10.0/helm-docs_0.10.0_Linux_x86_64.tar.gz"
-    sha256 "98e2bb4700bee3486c8f5b1a8df2e75aed58b3abc0692c6c441b6f65e1fbbea6"
+    url "https://github.com/norwoodj/helm-docs/releases/download/v0.11.0/helm-docs_0.11.0_Linux_x86_64.tar.gz"
+    sha256 "82bd866c5bc4167e2c6f100d7cbbc75db396a4c769cf83fcbf30b3ff8cdac5ff"
   end
 
   def install
